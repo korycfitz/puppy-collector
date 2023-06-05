953f176c9a8e5058a3175ee1e826acc88f27c946
korycfitz <korycfitz@gmail.com>
2023-06-05 13:14:52 -0400

    add static directory, add about template and view

--- main_app/views.py
+++ main_app/views.py
@@ -6,3 +6,6 @@ from django.http import HttpResponse
 # Define the home view
 def home(request):
   return HttpResponse('<h1>Test</h1>')
+
+def about(request):
+  return render(request, 'about.html')
