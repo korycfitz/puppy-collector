f2d740538a21cb7d833c2014f1cd3db39c7f94d6
korycfitz <korycfitz@gmail.com>
2023-06-05 11:00:06 -0400

    add mainapp in installed apps in settings.py

--- puppycollector/settings.py
+++ puppycollector/settings.py
@@ -34,12 +34,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+	'main_app',
+	'django.contrib.admin',
+	'django.contrib.auth',
+	'django.contrib.contenttypes',
+	'django.contrib.sessions',
+	'django.contrib.messages',
+	'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
