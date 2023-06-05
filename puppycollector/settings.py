65c64dd39b43b5d5c11bbc6167e6f8f53cc0b77c
korycfitz <korycfitz@gmail.com>
2023-06-05 09:59:26 -0400

    replace database connection settings in the Databases variable in settings.py

--- puppycollector/settings.py
+++ puppycollector/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'puppycollector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('BIT_IO_DATABASE_NAME'),
+        'USER': env('BIT_IO_USER'),
+        'PASSWORD': env('BIT_IO_PASSWORD'),
+        'HOST': 'db.bit.io',
+        'PORT': '5432',
     }
 }
 
