8b18a4834879d08794761e0316f9f4f537b06acf
korycfitz <korycfitz@gmail.com>
2023-06-05 09:58:20 -0400

    import Path to settings.py and import secret key from env

--- manage.py
+++ manage.py
@@ -3,9 +3,10 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'catcollector.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'puppycollector.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
