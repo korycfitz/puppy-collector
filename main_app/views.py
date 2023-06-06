0a7d33a7ba6a7035193c85cba7ab878de034a596
korycfitz <korycfitz@gmail.com>
2023-06-06 10:53:17 -0400

    fix bug with admin functionality

--- main_app/views.py
+++ main_app/views.py
@@ -12,17 +12,3 @@ def about(request):
 def puppy_index(request):
   puppies = Puppy.objects.all()
   return render(request, 'puppies/index.html', { 'puppies': puppies })
-
-class Puppy:  
-  def __init__(self, name, breed, description, age):
-    self.name = name
-    self.breed = breed
-    self.description = description
-    self.age = age
-
-puppies = [
-  Puppy('Snowball', 'maltese', 'old', 16),
-  Puppy('Bailey', 'rut', 'huge', 3),
-  Puppy('Bear', 'golden retriever', 'cute', 2),
-  Puppy('Cooper', 'german shepherd', 'scary', 10)
-]
