2d0b4badb187c6ffdc783e58dc7d2e4167b1db11
korycfitz <korycfitz@gmail.com>
2023-06-06 13:06:57 -0400

    add two new routes for update and delete functionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('puppies/', views.puppy_index, name='puppy-index'),
   path('puppies/<int:puppy_id>/', views.puppy_detail, name='puppy-detail'),
   path('puppies/create/', views.PuppyCreate.as_view(), name='puppy-create'),
+  path('puppies/<int:pk>/update/', views.PuppyUpdate.as_view(), name='puppy-update'),
+  path('puppies/<int:pk>/delete/', views.PuppyDelete.as_view(), name='puppy-delete'),
 ]
