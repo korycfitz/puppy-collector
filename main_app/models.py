8aefcc8ef5e8b323640e7e2ebf6ed53b4809781a
korycfitz <korycfitz@gmail.com>
2023-06-06 10:18:53 -0400

    migrate Puppy model

--- main_app/models.py
+++ main_app/models.py
@@ -5,3 +5,6 @@ class Puppy(models.Model):
   breed = models.CharField(max_length=75)
   description = models.TextField(max_length=300)
   age = models.IntegerField()
+
+  def __str__(self):
+    return self.name
