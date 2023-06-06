271d0cba74c2abf35d437148a63f6d38f89bb240
korycfitz <korycfitz@gmail.com>
2023-06-06 12:08:27 -0400

    add reverse method to puppy model and use to return the correct path in puppy-detail route

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Puppy(models.Model):
   name = models.CharField(max_length=50)
@@ -8,3 +9,6 @@ class Puppy(models.Model):
 
   def __str__(self):
     return self.name
+  
+  def get_absolute_url(self):
+    return reverse('puppy-detail', kwargs={'puppy_id': self.id})
