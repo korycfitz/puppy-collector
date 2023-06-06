13c442819b4c81dbc4301e74d6d0297b22ccca6b
korycfitz <korycfitz@gmail.com>
2023-06-06 19:18:47 -0400

    update corresponding view for home url

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('', views.Home.as_view(), name='home'),
   path('about/', views.about, name='about'),
   path('puppies/', views.puppy_index, name='puppy-index'),
   path('puppies/<int:puppy_id>/', views.puppy_detail, name='puppy-detail'),
