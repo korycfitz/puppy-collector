85568b2ba3c2a6748a62d54db1d05d17a5df01bd
korycfitz <korycfitz@gmail.com>
2023-06-06 10:16:11 -0400

    add model to models.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Puppy(models.Model):
+  name = models.CharField(max_length=50)
+  breed = models.CharField(max_length=75)
+  description = models.TextField(max_length=300)
+  age = models.IntegerField()
