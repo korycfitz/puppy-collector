a2b56eb1beef141790483a72b5cf047dfcbf5824
korycfitz <korycfitz@gmail.com>
2023-06-06 19:42:09 -0400

    import login and UserCreationForm from django

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView
 from .models import Puppy
