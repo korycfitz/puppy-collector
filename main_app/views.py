b7e707d598089ab90db64fd043caa865408df31b
korycfitz <korycfitz@gmail.com>
2023-06-22 18:58:15 -0400

    fix log in view

--- main_app/views.py
+++ main_app/views.py
@@ -9,32 +9,25 @@ from django.contrib.auth.views import LoginView
 from .models import Puppy
 
 
-def signup(request):
-  error_message = ''
-  if request.method == 'POST':
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in
-      login(request, user)
-      return redirect('home.html')
-    else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-  form = UserCreationForm()
-  context = {'form': form, 'error_message': error_message}
-  return render(request, 'signup.html', context)
-  # Same as: return render(request, 'signup.html', {'form': form, 'error_message': error_message})
-
-# Define the home view
 class Home(LoginView):
   template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('puppy-index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
 
 @login_required
 def puppy_index(request):
